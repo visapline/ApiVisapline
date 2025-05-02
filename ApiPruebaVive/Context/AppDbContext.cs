6ebe8ed87630718eef4cefc69c2d97f3d8c4ad0d
visapline <linevisap@gmail.com>
2025-05-02 18:06:02 -0500

    acomodando facturacion

--- ApiPruebaVive/Context/AppDbContext.cs
+++ ApiPruebaVive/Context/AppDbContext.cs
@@ -10,5 +10,7 @@ namespace ApiPruebaVive.Context
         // DbSet para la tabla 'Terceros' en la base de datos
         public DbSet<Terceros> Tercero { get; set; }
         public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Contrato> Contrato { get; set; }
+        public DbSet<Factura> Factura { get; set; }
     }
 }
