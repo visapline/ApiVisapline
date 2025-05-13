06e93b6a05145b251fcda7f7af7f406ed1e87e5b
visapline <linevisap@gmail.com>
2025-05-13 17:33:34 -0500

    Creamos controlador de contratos

---
+++ ApiPruebaVive/Controllers/ContratoesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiPruebaVive.Context;
+using ApiPruebaVive.Models;
+
+namespace ApiPruebaVive.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContratoesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ContratoesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contratoes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contrato>>> GetContrato()
+        {
+            return await _context.Contrato.ToListAsync();
+        }
+
+        // GET: api/Contratoes/estadisticas
+        [HttpGet("estadisticas")]
+        public async Task<ActionResult<IEnumerable<object>>> GetEstadisticasContratos()
+        {
+            var contratoCounts = await _context.Contrato
+                .GroupBy(c => c.EstadoC)
+                .Select(group => new
+                {
+                    Estado = group.Key,
+                    Cantidad = group.Count()
+                })
+                .ToListAsync();
+
+            return Ok(contratoCounts);
+        }
+
+        // GET: api/Contratoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contrato>> GetContrato(int id)
+        {
+            var contrato = await _context.Contrato.FindAsync(id);
+
+            if (contrato == null)
+            {
+                return NotFound();
+            }
+
+            return contrato;
+        }
+
+        // PUT: api/Contratoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContrato(int id, Contrato contrato)
+        {
+            if (id != contrato.IdContrato)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contrato).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContratoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contratoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Contrato>> PostContrato(Contrato contrato)
+        {
+            _context.Contrato.Add(contrato);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContrato", new { id = contrato.IdContrato }, contrato);
+        }
+
+        // DELETE: api/Contratoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContrato(int id)
+        {
+            var contrato = await _context.Contrato.FindAsync(id);
+            if (contrato == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contrato.Remove(contrato);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ContratoExists(int id)
+        {
+            return _context.Contrato.Any(e => e.IdContrato == id);
+        }
+    }
+}
